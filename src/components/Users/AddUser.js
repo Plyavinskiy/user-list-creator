5d2cf6a1953827850a7fccda897eb190c736ee8f
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-25 17:34:41 +0200

    feat: wrap the add users form into the card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from "../UI/Card";
+
 const AddUser = () => {
   const AddUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={AddUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
