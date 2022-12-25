8cc24478612cfd91220b533e68155633c7d9c195
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-25 22:07:47 +0200

    style: add styles for the Add User button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,6 +1,12 @@
+import classes from "./Button.module.css";
+
 const Button = (props) => {
   return (
-    <button type={props.type || "button"} onClick={props.onClick}>
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
