b79ff810d7bcc457a638ce4386845a84c9df5e1e
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-26 02:09:50 +0200

    style: add styles for the error modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,18 +1,19 @@
 import Card from "./Card";
 import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
     <div>
-      <div />
-      <Card>
-        <header>
+      <div className={classes.backdrop} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
           <h2>{props.title}</h2>
         </header>
-        <div>
+        <div className={classes.content}>
           <p>{props.message}</p>
         </div>
-        <footer>
+        <footer className={classes.actions}>
           <Button>OK</Button>
         </footer>
       </Card>
